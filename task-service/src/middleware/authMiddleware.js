21f176e8086398f7af2df3fb9dbf5089cf8cf450
kamitssm123 <kamitssm@gmail.com>
2025-03-02 22:16:50 +0530

    changed

--- task-service/src/middleware/authMiddleware.js
+++ task-service/src/middleware/authMiddleware.js
@@ -8,14 +8,14 @@ dotenv.config();
 export const authenticateJWT = async (req, res, next) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
-  const [blacklistedToken] = await sequelize.query(
-    'SELECT token FROM blacklisted_tokens WHERE token = :token',
-    { replacements: { token }, type: sequelize.QueryTypes.SELECT }
-  );
+  // const [blacklistedToken] = await sequelize.query(
+  //   'SELECT token FROM blacklisted_tokens WHERE token = :token',
+  //   { replacements: { token }, type: sequelize.QueryTypes.SELECT }
+  // );
 
-  if (blacklistedToken) {
-    return res.status(403).json({ message: 'Token is blacklisted. Please login again.' });
-  }
+  // if (blacklistedToken) {
+  //   return res.status(403).json({ message: 'Token is blacklisted. Please login again.' });
+  // }
   const secret_key = process.env.JWT_SECRET;
   if (token) {
     jwt.verify(token, secret_key, (err, user) => {
